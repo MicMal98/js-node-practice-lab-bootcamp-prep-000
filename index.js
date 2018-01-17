deb7da66b7f35a57c75a5d0fd0db4b36c9d48645
Michelle Donawa <donawamichelle@hotmail.com>
2018-01-17 23:15:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // write your code below!
 
 var name = "Michelle"
-var height = 62
+var height = "62"
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
-console.log(height + 4)
+console.log(parseInt(height) + 4)
